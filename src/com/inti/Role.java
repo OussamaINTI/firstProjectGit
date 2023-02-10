92807832f47e07e4e5b01483aae860a4553d5d8d
GerardAyg <aygun.gerard@gmail.com>
2023-02-10 16:16:53 +0100

    modification de l'attribut libelle

--- src/com/inti/Role.java
+++ src/com/inti/Role.java
@@ -2,24 +2,30 @@ package com.inti;
 
 public class Role {
 	private int id;
-	private String libelle;
+	private String libelllee;
+
 	public Role() {
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getLibelle() {
 		return libelle;
 	}
+
 	public void setLibelle(String libelle) {
 		this.libelle = libelle;
 	}
+
 	@Override
 	public String toString() {
 		return "Role [id=" + id + ", libelle=" + libelle + "]";
 	}
-	
+
 }
